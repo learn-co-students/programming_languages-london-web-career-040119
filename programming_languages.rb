80ec841d79dd192d9d5a1a72593ec14bae2f8c37
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-03-08 19:30:30 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,11 @@
-ddef lang_hash(languages)
+def lang_hash(languages)
   languages.each do |key, value|
-      puts value
+      
+
+      new_hash = {}
+      new_hash[:my_key] = value
+      puts new_hash
   end 
 end 
 
-values = lang_hash(languages)
-
-new_hash = {}
-
-new_hash[:my_key] = values
-puts new_hash
+lang_hash(languages)
