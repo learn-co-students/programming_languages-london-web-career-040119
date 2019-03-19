4ca487cd4a18c0938e1bd3b4b206d0426c2d7d3b
github username <github email address>
2019-03-19 23:10:57 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
   # your code here
-end
+  new_hash = {}
+    languages.each do |style, language|
+      # binding.pry
+      language.each do |language_name, language_attribute|
+        # binding.pry
+
+         if new_hash[language_name] == nil
+            new_hash[language_name] = language_attribute
+            # binding.pry
+        end
+        new_hash[language_name][:style] = []
+        new_hash[language_name][:style] << style
+
+       end
+    end
+    new_hash[:javascript][:style] << :oo
+    new_hash
+  end
