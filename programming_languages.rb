ff501f64f2074f39ce76abb435c555d1f4c5db1f
Francisco Costa <francisco@fcosta.pt>
2019-03-20 11:08:33 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+
+  new_hash = {}
+
+  language_names = (languages.collect {|styles, languages| languages.keys}).flatten.uniq
+
+  language_names.each {|language| new_hash[language.to_sym] = {:type => "", :style => []}}
+
+  languages.each do |styles, languages|
+    languages.each do |name, types|
+      language_names.each do |names|
+        if names == name
+          new_hash[name] = types
+        end
+      end
+    end
+  end
+
+  
+
 end
