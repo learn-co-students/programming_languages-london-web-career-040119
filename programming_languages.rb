b7a1badf8591d9cef2a4df6f617f35367750da19
Xabier C <xabier_abia@hotmail.com>
2019-03-21 14:15:04 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,13 @@ def reformat_languages(languages)
   languages.each do |type, hash|
     hash.each do |name, attributes|
       new_hash[name] ||= attributes
+      
+    # new_has[name] = sttributes if new_hash[name] == nil
+      
       new_hash[name][:style] ||= []
+      
+    # new_hash[name][:style] = [] if new_hash[name][:style] == nil
+      
       new_hash[name][:style] << type
     end
   end
