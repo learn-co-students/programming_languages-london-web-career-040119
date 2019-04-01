1c8a6f63fb17b7f3d247c82bd07aa8f5ef2aad27
Ay Has <ahashim983@yahoo.com>
2019-04-01 13:50:42 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,59 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  #get languages
+  k = languages[:oo].keys.concat(languages[:functional].keys)
+  t = []
+  k1 = languages[:oo].keys
+  k2 = languages[:functional].keys
+
+ #populating the hash
+ puts k
+ for item in k
+   new_hash[item] = {}
+ end
+ #insert type
+ for item in k1
+     new_hash[item] = {:type => languages[:oo][item][:type], :style => [:oo]}
+ end
+ for item in k2
+     new_hash[item] = {:type => languages[:functional][item][:type], :style => [:functional]}
+ end
+
+ new_hash[:javascript][:style].insert(0,:oo)
+ return new_hash
 end
+
+#reformat_languages(languages)
